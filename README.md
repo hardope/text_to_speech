6f044889967445190b19cefa0b5e3cddd726bbd4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-10-16 18:50:04 +0100

    All

---
+++ README.md
@@ -0,0 +1,2 @@
+# Text To Speech
+This is a python app to read the contents of your text files to you
---
+++ speech.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+import pyttsx3
+
+text = ""
+filename = input("filename: ")
+with open(filename, 'r') as file:
+	for i in file:
+		text = text + f"\n{i}"
+
+en = pyttsx3.init()
+en.say(text)
+en.runAndWait()
